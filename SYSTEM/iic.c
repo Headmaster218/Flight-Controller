2b4eba2ade75b2eb845e338a710242a289315e66
Unknown <20205571@stu.neu.edu.cn>
2022-05-17 14:41:47 +0800

    关闭初始化ACK
    解决自动ACK导致程序卡死，单字节读取仍有问题

--- SYSTEM/iic.c
+++ SYSTEM/iic.c
@@ -7,12 +7,27 @@
 
 void Soft_IIC1_Init(void)
 {					     
+	u8 i = 0;
 GPIO_InitTypeDef GPIO_InitStructure;
 I2C_InitTypeDef I2C_InitStructure;
 	
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
-RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2,ENABLE);
 	
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	for(;i<10;i++)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_10);
+		delay_us(10);
+		GPIO_ResetBits(GPIOB, GPIO_Pin_10);
+		delay_us(10);	
+	}
+	
+GPIO_DeInit(GPIOB);
+RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2,ENABLE);
 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
 GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -20,7 +35,7 @@ GPIO_Init(GPIOB, &GPIO_InitStructure);
 	
 I2C_DeInit(I2C2);
 I2C_InitStructure.I2C_Mode = I2C_Mode_I2C ; 
-I2C_InitStructure.I2C_Ack = I2C_Ack_Enable; 
+I2C_InitStructure.I2C_Ack = I2C_Ack_Disable; 
 I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
 I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
 I2C_InitStructure.I2C_ClockSpeed = 100000; 
@@ -145,9 +160,8 @@ while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_BYTE_TRANSMITTING))/////////////////
  delay++;
 }
 delay = 0;
-//I2C_GenerateSTOP(I2C2,ENABLE);
 I2C_GenerateSTART(I2C2,ENABLE);//起始信号
-while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT))
+while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT))//May be can delete
 {
  if(delay > 0x10000)
   return ERROR;
@@ -161,12 +175,19 @@ while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED))
   return ERROR;
  delay++;
 }	
-I2C_AcknowledgeConfig(I2C2,DISABLE);
+delay = 0;
+//while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_BYTE_RECEIVED))
+//{
+// if(delay > 0x10000)
+//  return ERROR;
+// delay++;
+//}
+//while((I2C2->CR2 & 0X40 ));
+delay_us(100);
 delay = I2C_ReceiveData(I2C2);
 I2C_GenerateSTOP(I2C2,ENABLE);
-I2C_AcknowledgeConfig(I2C2, ENABLE);
-return delay;
-}	
+	return delay;
+}
 
 //多字节读取
 	int I2C1_Soft_Mult_Read(u8 SlaveAddress,u8 REG_Address,u8 * ptChar,u8 size)
@@ -196,7 +217,7 @@ while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_BYTE_TRANSMITTING))/////////////////
  delay++;
 }
 delay = 0;
-//I2C_GenerateSTOP(I2C2,ENABLE);
+I2C_AcknowledgeConfig(I2C2, ENABLE);
 I2C_GenerateSTART(I2C2,ENABLE);//起始信号
 while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT))
 {
@@ -217,6 +238,5 @@ while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED))
  for(;size > 0; size--) *ptChar++ = I2C_ReceiveData(I2C2);
 I2C_AcknowledgeConfig(I2C2,DISABLE);
 I2C_GenerateSTOP(I2C2,ENABLE);
-I2C_AcknowledgeConfig(I2C2, ENABLE);
 return delay;
 }	
